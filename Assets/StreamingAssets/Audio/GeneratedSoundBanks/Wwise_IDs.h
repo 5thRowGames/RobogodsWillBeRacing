2e8c7c088816d923303ac0c60e2aa476a2b4a695
Eduardo <eduardo@sowoodrec.studio>
2019-06-07 10:39:48 +0300

    Wwise_Motor_Estable
    Segunda capa de motor estabilizada, cono emiter y eventos out de freno y turbo

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -16,8 +16,10 @@ namespace AK
         static const AkUniqueID BASCULA_COMPRESOR_IN = 3763551383U;
         static const AkUniqueID BASCULA_COMPRESOR_OUT = 3004065450U;
         static const AkUniqueID COCHE_1_FRENO = 2168487682U;
+        static const AkUniqueID COCHE_1_FRENO_OUT = 2410551647U;
         static const AkUniqueID COCHE_1_MOTOR_START_LOOP = 1441341373U;
         static const AkUniqueID COCHE_1_TURBO = 3520136878U;
+        static const AkUniqueID COCHE_1_TURBO_OUT = 3650669019U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS

e90a2b86bf1750cbda78498292d44b9a0a2cf697
Jose Cabello <jose.cabello.cubero@gmail.com>
2019-07-08 17:13:07 +0200

    Skin de botones de prueba y avance en el menu principal

---
+++ Assets/Scripts/utils/Tweens.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Tweens : MonoBehaviour
+{
+    
+    
+    public void ScaleTween(RectTransform rectTransform)
+    {
+        Vector3 tweenScale = rectTransform.localScale + new Vector3(.5f, .5f, .5f);
+        rectTransform.DOScale(tweenScale, 0.05f);
+    }
+
+}

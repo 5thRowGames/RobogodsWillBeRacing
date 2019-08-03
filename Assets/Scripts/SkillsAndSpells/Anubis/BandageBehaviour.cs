f523966a6e7006e1e3fd57a02778c8fe782ee2ca
Cabello9 <jose.cabello.cubero@gmail.com>
2019-08-03 20:04:56 +0200

    Lanzamiento de martillo y de venda. Hay que pulir y meter sus efectos

---
+++ Assets/Scripts/SkillsAndSpells/Anubis/BandageBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BandageBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //Aqui hay que meter el efecto
+        if (other.CompareTag("Algo"))
+        {
+            
+        }
+    }
+}

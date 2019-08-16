b1da6b07d7965a3493cf15af9ad5231513c9d279
Cabello9 <jose.cabello.cubero@gmail.com>
2019-08-16 17:36:06 +0200

    Prueba juntando trozos de circuito

---
+++ Assets/Scripts/JoinMaps/JoinMaps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinMaps : MonoBehaviour
+{
+   //public List<GameObject> mapParts;
+
+   public GameObject start;
+   public GameObject part1;
+   public GameObject part2;
+
+   private void Start()
+   {
+      CreateMap();
+   }
+
+   private void CreateMap()
+   {
+      Vector3 startJoin = start.transform.Find("JoinPoint").position;
+
+      GameObject part = Instantiate(part1, part1.transform.position, part1.transform.rotation);
+      GameObject partToJoin = part.transform.Find("StartJoin").gameObject;
+      partToJoin.transform.position = startJoin;
+      
+      startJoin = partToJoin.transform.Find("FinishJoin").position;
+      
+      part = Instantiate(part2, part2.transform.position, part2.transform.rotation);
+      partToJoin = part.transform.Find("StartJoin").gameObject;
+      partToJoin.transform.position = startJoin;
+      
+   }
+}

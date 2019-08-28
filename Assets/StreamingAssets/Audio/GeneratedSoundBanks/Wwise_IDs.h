acb8ec34b57979d5eff8212297bf9780eb8436b9
Eduardo <eduardo@sowoodrec.studio>
2019-08-28 09:27:43 +0300

    Cinematica_UI_Cajas
    boton de inicio de carrera, cinematica y cajas random

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -15,18 +15,27 @@ namespace AK
     {
         static const AkUniqueID BASCULA_COMPRESOR_IN = 3763551383U;
         static const AkUniqueID BASCULA_COMPRESOR_OUT = 3004065450U;
-        static const AkUniqueID COCHE_1_ACELERAR_IN = 2898692601U;
-        static const AkUniqueID COCHE_1_ACELERAR_OUT = 3666147384U;
-        static const AkUniqueID COCHE_1_ARRANQUE = 487895197U;
-        static const AkUniqueID COCHE_1_FRENO = 2168487682U;
+        static const AkUniqueID CAJA_RANDOM_IN = 3240997650U;
         static const AkUniqueID COCHE_1_FRENO_OUT = 2410551647U;
-        static const AkUniqueID COCHE_1_TURBO = 3520136878U;
-        static const AkUniqueID COCHE_1_TURBO_OUT = 3650669019U;
+        static const AkUniqueID FRENO_IN = 2731896591U;
         static const AkUniqueID IMPACTOS_IN = 2345257581U;
+        static const AkUniqueID INICIO_CARREA_IN = 616368701U;
         static const AkUniqueID MUSICA_LIMBO = 4092793509U;
+        static const AkUniqueID NAVE_1_ACELERAR_IN = 2922585971U;
+        static const AkUniqueID NAVE_1_ACELERAR_OUT = 2792057870U;
+        static const AkUniqueID NAVE_1_ARRANQUE = 3689912771U;
         static const AkUniqueID NAVE_2_ACELERAR_IN = 4135892272U;
         static const AkUniqueID NAVE_2_ACELERAR_OUT = 703537943U;
         static const AkUniqueID NAVE_2_ARRANQUE = 3067386190U;
+        static const AkUniqueID NAVE_3_ACELERAR_IN = 4187696677U;
+        static const AkUniqueID NAVE_3_ACELERAR_OUT = 693686188U;
+        static const AkUniqueID NAVE_3_ARRANQUE = 1350775313U;
+        static const AkUniqueID NAVE_4_ACELERAR_IN = 2760755186U;
+        static const AkUniqueID NAVE_4_ACELERAR_OUT = 3895169229U;
+        static const AkUniqueID NAVE_4_ARRANQUE = 1038650524U;
+        static const AkUniqueID TURBO_IN = 706776875U;
+        static const AkUniqueID TURBO_OUT = 2234672166U;
+        static const AkUniqueID UI_START_IN = 2335810608U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
@@ -43,7 +52,10 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID MOTOR_BUS = 252481087U;
+        static const AkUniqueID MOTOR_1_3_BUS = 453520701U;
+        static const AkUniqueID MOTOR_2_4_BUS = 2749669717U;
+        static const AkUniqueID MUSICA_BUS = 1054075596U;
+        static const AkUniqueID SFX_BUS = 213475909U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES

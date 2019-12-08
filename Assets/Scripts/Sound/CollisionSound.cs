354b42f7ae204a7108f204e14f3763e742af9d06
Cabello9 <jose.cabello.cubero@gmail.com>
2019-12-08 13:22:29 +0100

    AudioListeners actualizados

--- Assets/Scripts/Sound/CollisionSound.cs
+++ Assets/Scripts/Sound/CollisionSound.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 
 public class CollisionSound : MonoBehaviour
 {
+    public bool Anubis;
+    public bool Poseidon;
+    public bool Kali;
+    public bool Thor;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer != LayerMask.GetMask("Portal"))
+        if (other.CompareTag("Anubis") && Anubis)
         {
-            //AkSoundEngine.PostEvent("Impactos_In", gameObject);
+            AkSoundEngine.PostEvent("Impactos_In", gameObject);
         }
+        
+        if (other.CompareTag("Poseidon") && Poseidon)
+        {
+            AkSoundEngine.PostEvent("Impactos_In", gameObject);
+        }
+        
+        if (other.CompareTag("Kali") && Kali)
+        {
+            AkSoundEngine.PostEvent("Impactos_In", gameObject);
+        }
+        
+        if (other.CompareTag("Thor") && Thor)
+        {
+            AkSoundEngine.PostEvent("Impactos_In", gameObject);
+        }
+        
+        
     }
 }
